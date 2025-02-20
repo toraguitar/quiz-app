139e1b39cf32d93eda8c01f7faae45b6e9981db9
工藤景虎 <kagetora.kudo@nijibox.co.jp>
2025-02-20 19:59:05 +0900

    クイズ画面の静的見た目

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
-import Question from "./components/Question";
+"use client"
 
-export default function Home() {
+
+export default function QuizApp() {
   return (
-    <Question />
-  );
+    <div className="quizApp">
+      <h2 className="quizApp__title">
+        クイズアプリ
+      </h2>
+      <p>ゼクシィで1番古株なのは誰？</p>
+      <div className="">
+      </div>
+      <button className="">
+        次の問題へ
+      </button>
+    </div>
+  )
 }
+
